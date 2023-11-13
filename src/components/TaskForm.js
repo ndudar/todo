0af31b0f8bab00dde6e4a565f2d5f3ad0525c8c7
ndudar <ndudar@gmail.com>
2023-11-13 16:58:06 -0600

    TaskForm component shell

---
+++ src/components/TaskForm.js
@@ -0,0 +1,9 @@
+function TaskForm() {
+  return (
+    <div>
+      <p>this is a task form</p>
+    </div>
+  );
+}
+
+export default TaskForm;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,11 @@
 import TaskCard from "./TaskCard";
+import TaskForm from "./TaskForm";
 
 function TaskList() {
   return (
     <div>
       <p>this is the Task List</p>
+      <TaskForm />
       <TaskCard />
     </div>
   );
