35835aff76fe0867decc72ff7fa5775639b5c548
ndudar <ndudar@gmail.com>
2023-11-13 17:02:14 -0600

    UserLogin and UserRegistration component shells

---
+++ src/components/UserLogin.js
@@ -0,0 +1,9 @@
+function UserLogin() {
+  return (
+    <div>
+      <p>this where a user is authenticated</p>
+    </div>
+  );
+}
+
+export default UserLogin;
---
+++ src/components/UserRegistration.js
@@ -0,0 +1,9 @@
+function UserRegistration() {
+  return (
+    <div>
+      <p>this is where a user makes a new account</p>
+    </div>
+  );
+}
+
+export default UserRegistration;
