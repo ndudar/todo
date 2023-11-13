280ae7fe76785d7203750d409927ac6829dd8503
ndudar <ndudar@gmail.com>
2023-11-13 17:06:03 -0600

    SearchBar component shell

---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+function SearchBar() {
+  return (
+    <div>
+      <p>this is a search bar</p>
+    </div>
+  );
+}
+
+export default SearchBar;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,14 @@
 import TaskCard from "./TaskCard";
 import TaskForm from "./TaskForm";
 import TaskFilter from "./TaskFilter";
+import SearchBar from "./SearchBar";
 
 function TaskList() {
   return (
     <div>
       <p>this is the Task List</p>
       <TaskForm />
+      <SearchBar />
       <TaskFilter />
       <TaskCard />
     </div>
