e3c667777501dbad0f5430a6564d41a5e3c7f4a8
ndudar <ndudar@gmail.com>
2023-11-14 16:49:55 -0600

    initial style task card

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,7 +1,27 @@
+import { Card, Icon } from "semantic-ui-react";
+
 function TaskCard() {
   return (
     <div>
-      <p>this is a task card</p>
+      <Card>
+        <Card.Content>
+          <Card.Header>Task Card Title</Card.Header>
+          <Card.Meta>
+            <span>Due: Today</span>
+          </Card.Meta>
+          <Card.Description>This is the task description.</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Icon name="tag" />
+          Tag Here
+          <br />
+          <Icon name="user" />
+          User Assigned
+          <br />
+          <Icon name="flag" />
+          Status Flag
+        </Card.Content>
+      </Card>
     </div>
   );
 }
