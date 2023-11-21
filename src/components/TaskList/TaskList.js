371fd80628fd26076f1d187d1f4972e37fc12c03
ndudar <ndudar@gmail.com>
2023-11-20 18:20:14 -0600

    fixing task list styling

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -3,25 +3,25 @@ import TaskForm from "../TaskForm";
 import TaskFilter from "../TaskFilter";
 import SearchBar from "../SearchBar";
 
-import { Card, Container, Header } from 'semantic-ui-react';
+import { Card, Container, Header } from "semantic-ui-react";
 
-import './TaskList.css';
+import "./TaskList.css";
 
 function TaskList() {
   return (
     <div className="task-list-container">
       <div className="task-list-main">
-      <SearchBar />
-      <TaskFilter />
-      <Container>
-        <Header as="h2">My Tasks</Header>
-        <Card.Group centered>
-          <TaskCard />
-        </Card.Group>
-      </Container>
-      <div className="task-list-sidebar">
-      <TaskForm />
+        <SearchBar />
+        <TaskFilter />
+        <Container>
+          <Header as="h2">My Tasks</Header>
+          <Card.Group centered>
+            <TaskCard />
+          </Card.Group>
+        </Container>
       </div>
+      <div className="task-list-sidebar">
+        <TaskForm />
       </div>
     </div>
   );
